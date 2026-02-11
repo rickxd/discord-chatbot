169a2cb6fc7ec8cc2f63360f7fbd7edbc6b3940a
rickxd <rick.bomfim@hotmail.com>
2026-02-11 13:09:29 -0300

    Implementados 2 novos comandos e um handler para erros conhecidos.

--- bot/main.py
+++ bot/main.py
@@ -16,8 +16,26 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"O bot {bot.user} está conectado!")
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Você esqueceu de passar a mensagem.")
+    else:
+        raise error
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+
+@bot.command()
+async def echo(ctx, *, mensagem: str):
+    await ctx.send(mensagem)
+
+@bot.command()
+async def about(ctx):
+    await ctx.send(
+        "Olá! Sou um bot desenvolvido na linguagem python.\n"
+        "Possuo funções simples, sendo um projeto pessoal para aprender discord.py e boas maneiras."
+    )
 
 bot.run(TOKEN)
